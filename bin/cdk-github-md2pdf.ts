758d7f14c9b489044bfcc66ab0e5c6e9c66f7470
Rico Huijbers <huijbers@amazon.com>
2019-04-06 16:19:04 +0200

    Initial framework

---
+++ bin/cdk-github-md2pdf.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/cdk');
+import { CdkGithubMd2pdfStack } from '../lib/cdk-github-md2pdf-stack';
+
+const app = new cdk.App();
+new CdkGithubMd2pdfStack(app, 'CdkGithubMd2pdfStack');
